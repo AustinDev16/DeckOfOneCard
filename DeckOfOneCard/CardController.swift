f1902507e4e2266f1d74e390d09fb1a3f9617ac2
AustinDev16 <austin.blaser@gmail.com>
2016-09-01 21:36:17 -0600

    Card controller finished.

--- DeckOfOneCard/CardController.swift
+++ DeckOfOneCard/CardController.swift
@@ -16,7 +16,12 @@ class CardController {
         
         NetworkController.performRequestForURL(CardController.baseURL!, httpMethod: .Get, urlParameters: ["count":"\(numberOfCards)"]) { (data, error) in
             guard let data = data,
-            let jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)) as? [String:AnyObject] else { completion(card: []); return}
+            let jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)) as? [String:AnyObject],
+            let cardDictionaries = jsonDictionary["cards"] as? [[String:AnyObject]] else { completion(card: []); return}
+            
+            let cardsArray = cardDictionaries.flatMap({ Card(dictionary: $0)})
+            
+            completion(card: cardsArray)
             
         }
         
